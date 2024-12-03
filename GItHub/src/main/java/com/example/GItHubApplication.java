91dc25d6d82c7362b3d20fffde44ca570a07c096
tush210896 <trao86791@gmail.com>
2024-12-03 00:35:05 -0800

    'first'

---
+++ GItHub/src/main/java/com/example/GItHubApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GItHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GItHubApplication.class, args);
+	}
+
+}
